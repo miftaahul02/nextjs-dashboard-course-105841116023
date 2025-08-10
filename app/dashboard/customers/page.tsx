eabeef086e0fef3b65d98121a8e26d9589c97262
Firrofiqil a'la Kadram <105841115023@unismuh.student.ac.id>
2025-08-10 19:43:27 +0800

    ACCC LAHH

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -14,10 +14,7 @@ export const metadata: Metadata = {
 export default async function Page({
   searchParams,
 }: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-  };
+  searchParams?: { query?: string; page?: string };
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
